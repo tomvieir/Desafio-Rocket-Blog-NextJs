5827dd04a997f361b950dc2643d1bd39c271ba25
tomvieir <wevertonbvieira@gmail.com>
2022-04-29 19:12:07 -0300

    Header global add

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+import styles from './header.module.scss'
+
 export default function Header() {
-  // TODO
+  return(
+    <header>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+        <strong> <span>	&lt;/&gt;</span> spacetraveling <span>.</span> </strong>
+        </div>
+      </div>
+    </header>
+  )
 }
